0237d6e25dc88a82ff2bedc5cd5c72852e1b195f
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-09-04 23:13:46 +0200

    kind of works more

--- main.cc
+++ main.cc
@@ -16,7 +16,11 @@ using std::filesystem::directory_entry;
 struct MimeType {
     std::string media;
     std::string subType;
-    pugi::xml_document doc;
+    std::shared_ptr<pugi::xml_document> doc;
+
+    MimeType() {
+        doc = std::make_shared<pugi::xml_document>();
+    }
 };
 
 struct Magic {
@@ -48,10 +52,14 @@ struct Parser
     std::unordered_set<std::string> genericIcons;
     std::unordered_set<std::string> icons;
     std::unordered_set<std::string> globs;
+    std::unordered_set<std::string> xmlNamespaces;
 
 private:
+    void addMimetype(const std::string &type, MimeType &&mimeType, const pugi::xml_node &sourceNode);
+
     void parseAliases(const std::string &mimetype, const pugi::xml_node &node);
     void parseMagic(const pugi::xml_node &mimeNode);
+    void parseXMLNamespaces(const pugi::xml_node &mimeNode);
     void parseFields(
             const char *fieldname,
             const char *attribute,
@@ -110,6 +118,49 @@ void Parser::parseMagic(const pugi::xml_node &mimeNode)
 {
 }
 
+void Parser::parseXMLNamespaces(const pugi::xml_node &mimeNode)
+{
+}
+
+void Parser::addMimetype(const std::string &type, MimeType &&mimeType, const pugi::xml_node &sourceNode)
+{
+    std::unordered_map<std::string, MimeType>::iterator it =
+        mimeTypes.try_emplace(type, std::move(mimeType)).first;
+
+    pugi::xml_node mimeNode = it->second.doc->child("mime-type");
+    if (!mimeNode) {
+        mimeNode = it->second.doc->append_child("mime-type");
+    }
+
+    for (pugi::xml_attribute attr = sourceNode.first_attribute(); attr; attr = attr.next_attribute()) {
+        mimeNode.append_copy(attr);
+    }
+
+    for (pugi::xml_node child = sourceNode.first_child(); child; child = child.next_sibling()) {
+        const std::string name = child.name();
+        if (name == "magic") {
+            continue;
+        }
+        if (name == "glob") {
+            continue;
+        }
+        if (name == "root-XML") {
+            continue;
+        }
+        mimeNode.append_copy(child);
+    }
+
+    if (s_verbose) {
+        for (pugi::xml_attribute attr = sourceNode.first_attribute(); attr; attr = attr.next_attribute()) {
+            std::cout << " " << attr.name() << "=" << attr.value() << std::endl;;
+        }
+        int foo = 0;
+        if (foo++ < 10) {
+            it->second.doc->save(std::cout);
+        }
+    }
+}
+
 void Parser::loadSource(const filesystem::path &file)
 {
     pugi::xml_document doc;
@@ -141,24 +192,13 @@ void Parser::loadSource(const filesystem::path &file)
         MimeType mimeType;
         mimeType.media = type.substr(0, slashPos);
         mimeType.subType = type.substr(slashPos + 1);
-        mimeType.doc.append_copy(sourceNode);
 
         if (!knownMediaTypes.count(mimeType.media)) {
             std::cerr << "Unknown media type '" << mimeType.media << "'" << std::endl;
         }
 
-        if (s_verbose) {
-            for (pugi::xml_attribute attr = sourceNode.first_attribute(); attr; attr = attr.next_attribute()) {
-                std::cout << " " << attr.name() << "=" << attr.value() << std::endl;;
-            }
-            int foo = 0;
-            if (foo++ < 10) {
-                mimeType.doc.save(std::cout);
-            }
-        }
-        mimeTypes[type] = std::move(mimeType);
+        addMimetype(type, std::move(mimeType), sourceNode);
     }
-
 }
 
 void print_usage(const char *executable)
